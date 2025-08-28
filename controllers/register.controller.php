40fea32f4bd43d891e488bef524a7883f2884dbf
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-08-28 17:30:24 -0300

    Layout da tela de Login

--- controllers/register.controller.php
+++ controllers/register.controller.php
@@ -27,6 +27,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     exit();
 };
 
-header("Location: /login");
-
+view('register');
 exit();
---
+++ views/login.view.php
@@ -0,0 +1,43 @@
+<div class="grid grid-cols-2">
+  <div class="hero min-h-screen flex mx-auto">
+    <div class="hero-content m-auto py-8">
+      <div>
+        <p class="py-2 text-xl">Bem Vindo ao</p>
+        <h1 class="text-6xl font-bold">LockBox</h1>
+        <p class="py-2 pb-4 text-xl">Onde você guarda <span class="italic">tudo</span> com segurança</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="bg-white hero mr-40 min-h-screen text-black">
+    <div class="hero-content my-auto">
+      <form method="POST" action="/login">
+        <div class="card">
+          <div class="card-body">
+            <div class="card-title">Faça o seu login</div>
+            <label class="form-control mb-2">
+              <div class="label">
+                <span class="label-text text-black">Email</span>
+              </div>
+
+              <input type="text" class="input input-bordered w-full max-w-xs bg-gray-100" placeholder="Digite seu email" />
+            </label>
+
+            <label class="form-control">
+              <div class="label">
+                <span class="label-text text-black">Senha</span>
+              </div>
+
+              <input type="password" class="input input-bordered w-full max-w-xs bg-gray-100" placeholder="Digite sua senha" />
+            </label>
+
+            <div class="card-actions mt-4">
+              <button class="btn btn-primary btn-block">Login</button>
+              <a href="/register" class="btn btn-link px-0">Quero me registrar</a>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
