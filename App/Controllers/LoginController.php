d771628edc975b4cc04cd310dcbf128929ce9690
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-09-01 17:41:49 -0300

    LoginController e LogoutController

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -2,12 +2,46 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
+use Core\Database;
+use Core\Validation;
+
 class LoginController {
   public function index() {
     return view('login');
   }
 
   public function login() {
-    echo "loginController.login";
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $validation = Validation::validate([
+      'email' => ['required', 'email'],
+      'password' => ['required']
+    ], $_POST);
+
+    if ($validation->notValid()) {
+      return view('login');
+    }
+
+    $database = new Database(config('database'));
+
+    $user = $database->query(
+      query: "select * from users where email = :email",
+      class: User::class,
+      params: compact('email')
+    )->fetch();
+
+    if (! ($user && password_verify($_POST['password'], $user->password)) ) {
+      flash()->push('validations', ['email' => ['Usuário ou senha estão incorretos!']]);
+
+      return view('login');
+    }
+
+    $_SESSION['auth'] = $user;
+
+    flash()->push('message', "Seja bem-vindo " . $user->name . "!");
+
+    return redirect('/dashboard');
   }
 }
--- App/Controllers/LogoutController.php
+++ App/Controllers/LogoutController.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 class LogoutController {
   public function __invoke() {
-    echo "logoutController.__invoke";
+    session_destroy();
+
+    return redirect('/login');
   }
 }
