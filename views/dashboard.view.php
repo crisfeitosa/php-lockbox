d678304f2ee381222ae65f0be530a09d1cbd2e31
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-09-02 19:36:40 -0300

    Criando o layout da barra de pesquisa

--- views/dashboard.view.php
+++ views/dashboard.view.php
@@ -1,5 +1,5 @@
 <div class="mx-auto mx-auto max-w-screen-lg">
-  <div class="navbar bg-base-100 shadow-sm">
+  <div class="navbar bg-base-100">
     <div class="flex-1">
       <a class="btn btn-ghost text-xl">LockBox</a>
     </div>
@@ -16,5 +16,26 @@
         </li>
       </ul>
     </div>
+  </div>
+
+  <div class="flex space-x-4 w-full px-4">
+    <form class="w-full">
+      <label class="input input-bordered flex items-center gap-2 w-full">
+        <input type="text" class="grow" name="pesquisar" placeholder="Pesquisar notas no LockBox..." />
+        <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2.5"
+            fill="none"
+            stroke="currentColor">
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+      </label>
+    </form>
+
+    <a href="#" class="btn btn-primary">+ item</a>
   </div>
 </div>
